6f9327602729082673c16eefa93d7e16a7df7319
kyleMcG2000 <123557094+kyleMcG2000@users.noreply.github.com>
2023-11-23 11:55:57 +0000

    Gate collider works

--- Create with VR/VR Room - Kyle/Assets/SocketValidater.cs
+++ Create with VR/VR Room - Kyle/Assets/SocketValidater.cs
@@ -17,9 +17,9 @@ public class SocketValidater : MonoBehaviour
     public void OnTriggerEnter(Collider obj)
     {
 
-        if (obj.gameObject.name == "coffinRed" && gameObject.name == "coffinAttachRed1" || obj.gameObject.name == "coffinPink" && gameObject.name == "coffinAttachPink2"
-           || obj.gameObject.name == "coffinBlue" && gameObject.name == "coffinAttachBlue3" || obj.gameObject.name == "coffinGreen" && gameObject.name == "coffinAttachGreen4"
-           || obj.gameObject.name == "coffinYellow" && gameObject.name == "coffinAttachYellow5" || obj.gameObject.name == "coffinOrange" && gameObject.name == "coffinAttachOrange6")
+        if (obj.gameObject.name == "JackRed" && gameObject.name == "coffinAttachRed1" || obj.gameObject.name == "JackPink" && gameObject.name == "coffinAttachPink2"
+           || obj.gameObject.name == "jackBlue" && gameObject.name == "coffinAttachBlue3" || obj.gameObject.name == "JackGreen" && gameObject.name == "coffinAttachGreen4"
+           || obj.gameObject.name == "JackYellow" && gameObject.name == "coffinAttachYellow5" || obj.gameObject.name == "JackOrange" && gameObject.name == "coffinAttachOrange6")
         {
             SocketAttach.SetActive(true);
 
